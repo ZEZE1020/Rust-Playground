c70994ab5959e9ae231418999c29178f9a4cc607
Godfrey <ogembogodfrey75@gmail.com>
2025-03-02 23:10:09 +0300

    Set up Unit converter dirctory

---
+++ hello_cargo/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\RustPlayground\hello_cargo\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\user\Documents\RustPlayground\hello_cargo\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ unit-converter/src/units/mod.rs
@@ -0,0 +1,7 @@
+pub mod length;
+pub mod weight;
+pub mod temperature;
+
+pub use length::{Length, convert_length};
+pub use weight::{Weight, convert_weight};
+pub use temperature::{Temperature, convert_temperature};
